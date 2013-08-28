ed2c81563c74dd80310b40adc0fa63216a80e009
prabirshrestha <mail@prabir.me>
2013-08-28 16:32:33 -0400

    install curl and spf13-vim

--- NancyMono3.x/extras.sh
+++ NancyMono3.x/extras.sh
@@ -1,4 +1,12 @@
 
-" Install spf13-vim
-curl http://j.mp/spf13-vim3 -L -o - | sh
+apt-get update
+apt-get install -y curl
+apt-get install -y vim
+apt-get install -y git-core
+
+export AS_USER="sudo su vagrant -c"
+
+# Install spf13-vim
+$AS_USER "curl http://j.mp/spf13-vim3 -L -o - | sh"
+
 
