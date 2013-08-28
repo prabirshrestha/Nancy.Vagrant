c473c65c2fa0a91c65c6a4607067242dc0c5fc77
prabirshrestha <mail@prabir.me>
2013-08-28 17:45:43 -0400

    install zsh and tmux

--- NancyMono3.x/extras.sh
+++ NancyMono3.x/extras.sh
@@ -9,4 +9,13 @@ export AS_USER="sudo su vagrant -c"
 # Install spf13-vim
 $AS_USER "curl http://j.mp/spf13-vim3 -L -o - | sh"
 
+apt-get install -y zsh
+chsh vagrant -s /bin/zsh
+$AS_USER "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
+
+apt-get install -y tmux
+
+apt-get install -y python-pip
+$AS_USER "pip install --user git+git://github.com/Lokaltog/powerline"
+
 
