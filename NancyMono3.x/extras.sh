c9e1aaad5ac6b3af8f97bf2959dc9b0c46865277
prabirshrestha <mail@prabir.me>
2013-08-29 11:06:45 -0400

    install zsh in the end

--- NancyMono3.x/extras.sh
+++ NancyMono3.x/extras.sh
@@ -15,13 +15,6 @@ apt-get install powerline
 # Install spf13-vim
 $AS_USER "curl http://j.mp/spf13-vim3 -L -o - | sh"
 
-# Install zsh
-apt-get install -y zsh
-# Set zsh as default shell
-chsh vagrant -s $(which zsh)
-# Install oh-my-zsh
-$AS_USER "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
-
 # Install tmux
 apt-get install -y tmux
 gem install tmuxinator
@@ -40,6 +33,15 @@ $AS_USER "cp -rf .vimrc.bundles.local ~/.vimrc.bundles.local"
 # Install cmake which is required to build YouCompleteMe
 apt-get install -y cmake
 apt-get install -y python-dev
+export PATH="$PATH:/opt/mono/bin"
+export LD_LIBRARY_PATH="/opt/mono/lib"
 $AS_USER "(cd ~/.vim/bundle; git clone https://github.com/Valloric/YouCompleteMe.git; cd YouCompleteMe; ./install.sh --omnisharp-completer)"
-$AS_USER "vim +BundleClean! +BundleInstall! +q"
+$AS_USER "vim +BundleInstall! +BundleClean! +q"
+
+# Install zsh
+apt-get install -y zsh
+# Set zsh as default shell
+chsh vagrant -s $(which zsh)
+# Install oh-my-zsh
+$AS_USER "curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh"
 
